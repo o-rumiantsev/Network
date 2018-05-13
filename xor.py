22e39e44ad09f947217a3c153df27334dffe8eea
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2018-05-13 11:32:00 +0300

    Improved test

--- xor.py
+++ xor.py
@@ -4,19 +4,18 @@ model = Model(0.9)
 
 model.add(2)
 model.add(10)
-model.add(1)
+model.add(2)
 
 
-print(model.run([0, 1]))
-print(model.run([1, 1]))
+trainSet = [[[0, 1], [1, 0]], [[1, 0], [1, 0]], [[0, 0], [0, 1]], [[1, 1], [0, 1]]]
+model.train(trainSet, 7000)
 
-trainSet = [[[0, 1], [1]], [[1, 0], [1]], [[0, 0], [0]], [[1, 1], [0]]]
+test = [[0, 1], [1, 0], [0, 0], [1, 1], [0.5, 0.5], [1, 0.5], [0.5, 1], [1, 0.2]]
 
-model.train(trainSet, 10000)
-
-print()
-print(model.run([0, 1]))
-print(model.run([1, 1]))
-
-print(model.run([1, 0]))
-print(model.run([0, 0]))
+for i in range(len(test)):
+    res = model.run(test[i])
+    diff = res[0] - res[1]
+    if diff > 0:
+        print('Return 1 ' + str(test[i]) + ' ' + str(int(res[0] * 100)) + '%')
+    else:
+        print('Return 0 ' + str(test[i]) + ' ' + str(int(res[1] * 100)) + '%')
